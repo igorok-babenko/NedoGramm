5ec6eb1b39eeeaabf9b222b3dacfd3163252f5ee
Igor Babenko <igorok.babenko@gmail.com>
2021-07-08 11:36:10 +0300

    Added posts and views

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'bootstrap', '~> 4.1.1'
 gem 'jquery-rails'
 gem 'font-awesome-rails'
 gem 'devise'
+gem 'kaminari'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:new, :create]
 end
